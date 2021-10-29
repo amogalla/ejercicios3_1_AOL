c222af0d7fe01199a67cb348f1e482c70e5dbbb9
Alejandro <amogalla@iesrafaelalberti.es>
2021-10-29 15:52:38 +0200

    Código simplificado en las funciones calcularArea y calcularPerimetro.

--- src/main/kotlin/ejercicio5_6.kt
+++ src/main/kotlin/ejercicio5_6.kt
@@ -2,16 +2,16 @@ class Rectangulo(b: Int = 1, h: Int = 1) {
     var base = b
     var altura = h
 
-    fun calcularArea(): Int {return base * altura}
-    fun calcularPerimetro(): Int {return (base + altura) * 2}
+    fun calcularArea(): Int = base * altura
+    fun calcularPerimetro(): Int = (base + altura) * 2
 
     override fun toString():String{return "Base = $base, altura = $altura"}
 }
 
 fun main(args: Array<String>) {
-    var r1 = Rectangulo(3, 4)
-    var r2 = Rectangulo(1, 5)
-    var r3 = Rectangulo(8, 10)
+    val r1 = Rectangulo(3, 4)
+    val r2 = Rectangulo(1, 5)
+    val r3 = Rectangulo(8, 10)
     println("Área de R1 = " + (r1.calcularArea()))
     println("Perímetro de R1 = " + r1.calcularPerimetro())
 
