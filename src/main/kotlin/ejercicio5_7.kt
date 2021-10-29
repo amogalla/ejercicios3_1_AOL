eac11d67639abcdfbbebb6091b47d2dc031105f0
Alejandro <amogalla@iesrafaelalberti.es>
2021-10-29 16:25:13 +0200

    Clase coche simplificada. Variable caballos corregida.

--- src/main/kotlin/ejercicio5_7.kt
+++ src/main/kotlin/ejercicio5_7.kt
@@ -1,21 +1,14 @@
-class Coche(matricula:String, color:String = "", marca:String = "", modelo:String = "", cavallos:Int = 100, numPuertas:Int = 0) {
-    var color = color
-    var marca = marca
-    var modelo = modelo
-    var cavallos = cavallos
-    var numPuertas = numPuertas
-    var matricula = matricula
-
+class Coche(var matricula:String, var color:String = "", var marca:String = "", var modelo:String = "", var caballos:Int = 100, var numPuertas:Int = 0) {
     override fun toString():String{
-        return "Marca = $marca, modelo = $modelo, color = $color, nº de puertas = $numPuertas, matrícula = $matricula, CV = $cavallos"
+        return "Marca = $marca, modelo = $modelo, color = $color, nº de puertas = $numPuertas, matrícula = $matricula, CV = $caballos"
     }
 }
 
 fun main(args: Array<String>) {
-    var c1 = Coche("1917LQ","Rojo", "Ferrari", "Vento", 300, 3 )
-    var c2 = Coche("1111AA","Gris", "Renault", "Laguna", 250, 5)
-    var c3 = Coche("1234ZZ","Negro", "Peugeot", "P4", 110, 4)
-    var c4 = Coche("8765RT")
+    val c1 = Coche("1917LQ","Rojo", "Ferrari", "Vento", 300, 3 )
+    val c2 = Coche("1111AA","Gris", "Renault", "Laguna", 250, 5)
+    val c3 = Coche("1234ZZ","Negro", "Peugeot", "P4", 110, 4)
+    val c4 = Coche("8765RT")
 
     println("Estos son los coches originales:")
     println(c1)
